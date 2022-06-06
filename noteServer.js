498178acbaf4fc3f94db4978fb11f8349f23fc5c
Jack Chamberlain <jack.chamberlain11@yahoo.com>
2022-06-06 14:46:29 -0500

    server added

--- noteServer.js
+++ noteServer.js
@@ -2,3 +2,16 @@ const express = require('express');
 const BEroutes = require('./BEroutes');
 const FEroutes = require('./FEroutes');
 const app = express();
+const BEroutes = require('./Dataroutes/BEroutes');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', BEroutes);
+app.use('/api', FEroutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+module.exports = app;
